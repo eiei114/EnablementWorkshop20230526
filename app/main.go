83ac3ea7f85987e681d6f726b5c08d8b01652042
eiei114 <nowaka37564@gmail.com>
2023-05-26 18:53:13 +0900

    feat 並行処理実装

--- app/main.go
+++ app/main.go
@@ -2,15 +2,28 @@ package main
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	jpeg "image/jpeg"
 	png "image/png"
 	"os"
 	"path/filepath"
+	"runtime/trace"
+	"sync"
 )
 
 func main() {
+	f, err := os.Create("trace.out")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	err = trace.Start(f)
+	if err != nil {
+		panic(err)
+	}
+	defer trace.Stop()
+
 	// ワイルドカードを展開してファイルリストを取得
 	files, err := filepath.Glob(os.Args[1])
 	if err != nil {
@@ -24,18 +37,40 @@ func main() {
 		os.Exit(1)
 	}
 }
+
 func run(ctx context.Context, files []string) error {
-	//ファイルを取得
-	for _, file := range files {
-		//変換関数を呼ぶ
-		if err := convert(ctx, file); err != nil {
-			return err
-		}
+	if err := convertAll(ctx, files); err != nil {
+		return err
 	}
 	return nil
 }
 
+func convertAll(ctx context.Context, files []string) error {
+	var wg sync.WaitGroup
+	ctx, task := trace.NewTask(ctx, "convert All")
+	defer task.End()
+
+	for _, file := range files {
+		wg.Add(1)
+		go func(file string) {
+			defer wg.Done()
+			//convertを呼ぶ
+			if err := convert(ctx, file); err != nil {
+				fmt.Println(err) //エラーを表示す
+			}
+		}(file)
+	}
+	wg.Wait()
+	//成功したらnilを返す
+	return nil
+}
+
 func convert(ctx context.Context, file string) (rerr error) {
+	region := trace.StartRegion(ctx, "convert")
+	defer region.End()
+
+	//以下は元のコードと同じ...
+
 	//ファイルを開く
 	src, err := os.Open(file)
 	if err != nil {
@@ -64,7 +99,7 @@ func convert(ctx context.Context, file string) (rerr error) {
 		dst.Close()
 		if rerr != nil {
 			//失敗したらファイルを削除
-			rerr = errors.Join(rerr, os.Remove(jpgfile))
+			os.Remove(jpgfile)
 		}
 	}()
 	//jpgファイルにエンコード
